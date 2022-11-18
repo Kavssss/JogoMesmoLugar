46b98df61d2000729b7902261743eea361ac111c
Flávio Morais <108704642+Kavssss@users.noreply.github.com>
2022-11-18 16:48:54 -0300

    bloco

--- Scripts/Entrada.gd
+++ Scripts/Entrada.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
 func _on_Entrada_body_entered(body):
-	if(get_tree().get_current_scene().get_name() == "4-Volte"):
+	if get_tree().get_current_scene().get_name() == "4-Volte":
 		get_node("/root/Global").set("fase", 5)
 		get_node("/root/Transition").fade_into("res://Cenas/5-PrecisoDeEspaco.tscn")
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var dicas = 3
 var posicao = Vector2(32, 544)
-var fase = 17
+var fase = 1
 var fase_atual = 25
 var nomes = ["1-Simples", "2-Espere", "3-CadeOBotao", "4-Volte", "5-PrecisoDeEspaco", "6!", 
 			"7-Confusao", "8-MaisForca", "9-Lua", "10-Sozinho", "11-EncontreOX", "12-SigaORato", 
