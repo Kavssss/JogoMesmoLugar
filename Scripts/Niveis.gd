3eac23df2ddd8aa20dc82210f0451d9a650c50c5
Flávio Morais <108704642+Kavssss@users.noreply.github.com>
2022-11-23 15:08:39 -0300

    *

--- Scripts/Niveis.gd
+++ Scripts/Niveis.gd
@@ -1,52 +1,13 @@
 extends Node2D
 
-var posX = 352
-var posY = 64
+var posX = [0, 352, 480, 608, 736, 864, 992, 1120, 992, 864, 736, 608, 480, 352, 224, 352, 480, 608, 736, 864, 992, 1120]
+var posY = [0, 64, 64, 64, 64, 64, 64, 288, 288, 288, 288, 288, 288, 288, 512, 512, 512, 512, 512, 512, 512, 512]
 
 func _process(delta):
 	if get_node("/root/Global").fase > laser():
 		get_node("/root/Global").set("laser", get_node("/root/Global").fase)
-
-	if laser() == 1:
-		get_node("Laser").position = Vector2(posX, posY)
-	elif laser() == 2:
-		get_node("Laser").position = Vector2(posX + 128, posY)
-	elif laser() == 3:
-		get_node("Laser").position = Vector2(posX + (128 * 2), posY)
-	elif laser() == 4:
-		get_node("Laser").position = Vector2(posX + (128 * 3), posY)
-	elif laser() == 5:
-		get_node("Laser").position = Vector2(posX + (128 * 4), posY)
-	elif laser() == 6:
-		get_node("Laser").position = Vector2(posX + (128 * 5), posY)
-	elif laser() == 7:
-		get_node("Laser").position = Vector2(posX + (128 * 6), posY)
-	elif laser() == 8:
-		get_node("Laser").position = Vector2(posX + (128 * 5), posY + 224)
-	elif laser() == 9:
-		get_node("Laser").position = Vector2(posX + (128 * 4), posY + 224)
-	elif laser() == 10:
-		get_node("Laser").position = Vector2(posX + (128 * 3), posY + 224)
-	elif laser() == 11:
-		get_node("Laser").position = Vector2(posX + (128 * 2), posY + 224)
-	elif laser() == 12:
-		get_node("Laser").position = Vector2(posX + 128, posY + 224)
-	elif laser() == 13:
-		get_node("Laser").position = Vector2(posX, posY + 224)
-	elif laser() == 14:
-		get_node("Laser").position = Vector2(posX - 128, posY + (224 * 2))
-	elif laser() == 15:
-		get_node("Laser").position = Vector2(posX, posY + (224 * 2))
-	elif laser() == 16:
-		get_node("Laser").position = Vector2(posX + 128, posY + (224 * 2))
-	elif laser() == 17:
-		get_node("Laser").position = Vector2(posX + (128 * 2), posY + (224 * 2))
-	elif laser() == 18 or laser() == 19:
-		get_node("Laser").position = Vector2(posX + (128 * 4), posY + (224 * 2))
-	elif laser() == 20:
-		get_node("Laser").position = Vector2(posX + (128 * 5), posY + (224 * 2))
-	elif laser() == 21:
-		get_node("Laser").position = Vector2(posX + (128 * 6), posY + (224 * 2))
+	if laser() > 0:
+		get_node("Laser").position = Vector2(posX[laser()], posY[laser()])
 	else:
 		get_node("Laser").hide()
 		get_node("Laser/CollisionShape2D").disabled = true
@@ -57,4 +18,3 @@ func laser():
 func _on_Voltar_body_entered(body):
 	get_node("/root/Transition").fade_into("res://Cenas/Menu.tscn")
 	get_node("/root/Global").posicao_menu(544, 544)
-	
